004f75c20bb549fdff34065d71ef73e84a34699a
glucas038 <lucas-gabriel70@live.com>
2023-02-20 11:12:49 -0300

    Primeira versão

---
+++ bootcamp/sintaxejava/tipos_e_variaveis/TiposVariaveis.java
@@ -0,0 +1,7 @@
+package edu.lucas.um.bootcamp.sintaxejava.tipos_e_variaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
