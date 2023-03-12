f52f8ca73e1725e9272fed7eeb6c650ccd0754fb
glucas038 <lucas-gabriel70@live.com>
2023-03-12 09:42:32 -0300

    Estruturas de dados prontas

---
+++ bootcamp/estrutura_de_dados/map/Carro.java
@@ -0,0 +1,48 @@
+package edu.lucas.um.bootcamp.estrutura_de_dados.map;
+
+import java.util.Objects;
+
+public class Carro {
+
+    String marca;
+    String cor;
+
+    public Carro(String marca) {
+        this.marca = marca;
+        this.cor = null;
+    }
+
+    public Carro(String marca, String cor) {
+        this.marca = marca;
+        this.cor = cor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carro carro = (Carro) o;
+        return Objects.equals(marca, carro.marca) && Objects.equals(cor, carro.cor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(marca);
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "marca='" + marca + '\'' +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+}
