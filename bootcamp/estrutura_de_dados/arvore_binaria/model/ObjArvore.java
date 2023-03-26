c714373822ddcde06a22ebd0a5e4e2eb29f6cccb
glucas038 <lucas-gabriel70@live.com>
2023-03-26 17:23:53 -0300

    Reorganização de pastas e Collections

--- bootcamp/estrutura_de_dados/arvore_binaria/model/ObjArvore.java
+++
@@ -1,9 +0,0 @@
-package edu.lucas.um.bootcamp.estrutura_de_dados.arvore_binaria.model;
-
-public abstract class ObjArvore <T> implements Comparable<T>{
-
-    public abstract boolean equals(Object o);
-    public abstract int hashCode();
-    public abstract int compareTo(T outro);
-    public abstract String toString();
-}
